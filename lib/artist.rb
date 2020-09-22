12166bb390793360435d5b8bad8f636a16fd6cff
Leah Schlackman <leahemily1@gmail.com>
2020-09-22 16:51:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,9 +3,17 @@ class Artist
   
   def initialize(name)
     @name = name 
+    @songs = []
   end 
   
   def songs 
     Song.all.select{|s| s.artist == self}
   end 
+  
+  def add_song(song_obj)
+    @songs << song_obj
+    song_obj.artist = self
+  end 
+  
+    
 end 
