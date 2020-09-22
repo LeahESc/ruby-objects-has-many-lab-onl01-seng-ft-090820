6ba6caa3bd973a5dcb27bfe70c129f681d9a0a52
Leah Schlackman <leahemily1@gmail.com>
2020-09-22 17:45:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,8 @@
 class Artist 
-  attr_accessor :name 
+  attr_accessor :artist_name 
   
-  def initialize(name)
-    @name = name 
+  def initialize(artist_name)
+    @artist_name = artist_name 
     @songs = []
   end 
   
@@ -15,9 +15,9 @@ class Artist
     song_obj.artist = self
   end 
   
-  def add_song_by_name(title)
-    song = Song.new(title)
-    song.title = title 
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.name = name 
     song.artist = self
     @songs << song 
   end 
